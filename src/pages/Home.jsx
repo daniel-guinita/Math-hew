5ec4edce07f14794ccc0d69472693fc6be300cb1
Daniel Guinita <daniel.guinita@cit.edu>
2024-11-12 13:51:22 +0800

    dont merge yet front end changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Home.css';
 
 const Home = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center px-4 py-8 dark:bg-gray-900">
-      <div className="flex flex-col md:flex-row items-center justify-between w-full max-w-6xl">
-        <div className="md:w-1/2">
-          <h1 className="text-5xl font-bold text-gray-800 dark:text-white mb-6">
-            Math-hew
-          </h1>
-          <p className="text-xl text-gray-600 dark:text-gray-300 mb-8">
-            Your fun guide in 4th grade Mathematics!
-          </p>
-          <Link to="/sign-in" className="inline-block">
-            <button className="bg-orange-500 text-white font-bold py-2 px-4 rounded hover:bg-orange-700 transition duration-300">
-              Learn Now
-            </button>
+    <div className="home-container">
+      <div className="home-content">
+        <div className="home-text">
+          <h1 className="home-title">Welcome to Math-hew!</h1>
+          <p className="home-description">Your fun guide to 4th grade Mathematics!</p>
+          <Link to="/sign-in" > {/* No need for inline-block, Link is already inline */}
+            <button className="learn-button">Learn Now</button>
           </Link>
         </div>
 
-        <div className="md:w-1/2">
+        <div className="home-image">
           <img
             src="/images/mathhew.png" 
             alt="Math-hew Mascot"
-            className="w-full max-w-lg h-auto mx-auto"
           />
         </div>
       </div>
